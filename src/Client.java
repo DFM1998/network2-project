a4b2cdc8de659ae8f14af39a789c111464fc69c0
David Matos Freitas <davidfreitas-98@hotmail.com>
2021-05-11 21:09:26 +0200

    Clean up of the code

--- src/Client.java
+++ src/Client.java
@@ -7,34 +7,24 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-// commands to test:
-// SET:8000:HALLO:TEST
-// GET:8000:HALLO
-// SET:8001:YES:TEST2
-// CHECK:YES:5
-// SET:8007:NO:HALLO
-// SET:8009:NO:HALLO
-// SET:8008:NO:HALLO
-
 public class Client implements Runnable {
     public void connectToServer(int serverId, String outputString) {
         try {
             // create a connection
             Socket s = new Socket("127.0.0.1", serverId);
-            // add client port to the string
-            //outputString += ":" + s.getLocalPort();
+
             // send the information to the server
             DataOutputStream output = new DataOutputStream(s.getOutputStream());
+
             // send the input to the server
             output.writeUTF(outputString);
+
             // get the information from the server
             DataInputStream input = new DataInputStream(s.getInputStream());
+
             // print the output that we got from the server
             String serverAnswer = input.readUTF();
             System.out.println(serverAnswer);
-            if (outputString.substring(0, 5).equalsIgnoreCase("CHECK")) {
-                System.out.println(input.readUTF());
-            }
 
             // close and flush the connection
             output.flush();
@@ -53,8 +43,11 @@ public class Client implements Runnable {
             Scanner scanner = new Scanner(System.in);
             String inputString = scanner.nextLine();
 
+            if (inputString.length() < 3){
+                System.out.println("Please enter any command: GET, SET or CHECK.");
+            }
             // check the user input is valid
-            if (inputString.substring(0, 3).equalsIgnoreCase("GET")) {
+            else if (inputString.substring(0, 3).equalsIgnoreCase("GET")) {
                 Pattern regex = Pattern.compile("(GET):[0-9]{1,7}:[a-zA-Z0-9_]+", Pattern.CASE_INSENSITIVE);
                 Matcher message = regex.matcher(inputString);
                 if (message.find()) {
@@ -63,7 +56,7 @@ public class Client implements Runnable {
                     // send the information to the server
                     connectToServer(serverId, inputString.toUpperCase());
                 } else {
-                    System.out.println("Not right format: GET:<id>:<key>");
+                    System.out.println("Not right format: GET:<port>:<key>");
                 }
             } else if (inputString.substring(0, 3).equalsIgnoreCase("SET")) {
                 Pattern regex = Pattern.compile("(SET):[0-9]{1,7}:[a-zA-Z0-9_]+:[a-zA-Z0-9_]+", Pattern.CASE_INSENSITIVE);
@@ -79,7 +72,7 @@ public class Client implements Runnable {
                     // send the information to the server
                     connectToServer(serverId, inputString.toUpperCase());
                 } else {
-                    System.out.println("Not right format: SET:<id>:<key>:<value> (Do not use ':' for the <value>)");
+                    System.out.println("Not right format: SET:<port>:<key>:<value> (Do not use ':' for the <value>)");
                 }
             } else if (inputString.substring(0, 5).equalsIgnoreCase("CHECK")) {
                 Pattern regex = Pattern.compile("(CHECK):[a-zA-Z0-9_]+", Pattern.CASE_INSENSITIVE);
@@ -91,6 +84,7 @@ public class Client implements Runnable {
                     //time to live is 5 inorder to check all the nodes for the information
                     int ttl = 5;
                     inputString += ":" + ttl + ":" + serverId;
+                    System.out.println("Searching on the servers...");
                     // send the information to the server
                     connectToServer(serverId, inputString.toUpperCase());
                 } else {
--- src/Server.java
+++ src/Server.java
@@ -1,11 +1,7 @@
-import javax.sound.sampled.Port;
-import java.awt.*;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.PrintStream;
 import java.net.*;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
@@ -36,24 +32,26 @@ public class Server implements Runnable {
             //output is the information send back to the client
             DataOutputStream outputStream = new DataOutputStream(clientSocket.getOutputStream());
             // convert UTF to string
-            String str = (String) dis.readUTF();
+            String str = dis.readUTF();
 
             System.out.println("Client (" + clientSocket.getPort() + ") said to server: " + str);
 
-            boolean checkOtherServer = false;
             // check if was a get or a set
             if (str.substring(0, 3).equalsIgnoreCase("GET")) {
                 String[] splitMessage = str.split(":");
                 String key = splitMessage[2];
                 String output = getValue(key);
+                // check if key has been found
+                if (output == null) {
+                    output = "Server ("+ss.getLocalPort()+"): Key does not exist";
+                }
                 outputStream.writeUTF(output);
             } else if (str.substring(0, 3).equalsIgnoreCase("SET")) {
-                // TODO Test if the same key overwrites the first one
                 String[] splitMessage = str.split(":");
                 String key = splitMessage[2];
                 String value = splitMessage[3];
                 putStorage(key, value);
-                outputStream.writeUTF("Server: information has been saved.");
+                outputStream.writeUTF("Server ("+ss.getLocalPort()+"): information has been saved.");
             } else if (str.substring(0, 5).equalsIgnoreCase("CHECK")) {
                 // broadcast command
                 String[] splitMessage = str.split(":");
@@ -62,23 +60,58 @@ public class Server implements Runnable {
                 portNumber = Integer.parseInt(splitMessage[3]);
                 ttl--;
                 str = "CHECK:" + key + ":" + ttl + ":" +portNumber;
-                outputStream.writeUTF("Server: searching on the servers...");
+                // check if this server has the value for the corresponding key
                 if (storage.containsKey(key)) {
+                    // in case the first server has the information, then he will send it directly to the client
                     if ((ss.getLocalPort()) == portNumber) {
-                        outputStream.writeUTF(getValue(key));
+                        outputStream.writeUTF("Server ("+ss.getLocalPort()+"):"+getValue(key));
                     } else {
                         // if found the key, then let's send it the main server
                         Socket mainServer = new Socket("127.0.0.1", portNumber);
                         DataOutputStream mainServerOutput = new DataOutputStream(mainServer.getOutputStream());
-                        mainServerOutput.writeUTF("FOUND:" + getValue(key));
+                        mainServerOutput.writeUTF("FOUND:" + ss.getLocalPort() + ":" + getValue(key));
                         mainServerOutput.flush();
                         mainServerOutput.close();
                         mainServer.close();
                     }
                 } else {
+                    // check if packet should die or not
                     if (ttl > 0) {
-                        checkOtherServer = true;
+                        // start a connection between the nodes
+                        serverConnection(ss);
+                        // send information to my nodes
+                        for (Socket server : servers) {
+                            System.out.println(server);
+                            DataOutputStream output = new DataOutputStream(server.getOutputStream());
+                            output.writeUTF(str);
+                            server.close();
+                        }
+                        // only the server who got the check message from the client, should send an answer to the client
+                        if (ss.getLocalPort() == portNumber) {
+                            int i = 0;
+                            String answerToClient = "The package died, because the TTL ran out of time...";
+                            // while until 16 because the TTL is 4, so that we make sure that each server got asked if he has the key or not
+                            while (i<16) {
+                                // the server creates a new connection with a node and gets an answer from the node
+                                // repeat for all the nodes
+                                Socket waitNode = ss.accept();
+                                DataInputStream dis2 = new DataInputStream(waitNode.getInputStream());
+                                String str2 = dis2.readUTF();
+                                i++;
+                                // close the connection with the node
+                                waitNode.close();
+                                // as soon as the key is found, we break the while and give the client the information
+                                if(str2.substring(0, 5).equalsIgnoreCase("FOUND")) {
+                                    String[] splitMessage2 = str2.split(":");
+                                    System.out.println("Information has been found on the server ("+splitMessage2[2]+")");
+                                    answerToClient = "Server ("+splitMessage2[1]+"): "+splitMessage2[2];
+                                    break;
+                                }
+                            }
+                            outputStream.writeUTF(answerToClient);
+                        }
                     } else {
+                        //inform the server that asked for the information that the packet died
                         Socket mainServer = new Socket("127.0.0.1", portNumber);
                         DataOutputStream mainServerOutput = new DataOutputStream(mainServer.getOutputStream());
                         mainServerOutput.writeUTF("TTL ran out of time...");
@@ -86,36 +119,6 @@ public class Server implements Runnable {
                         mainServerOutput.close();
                         mainServer.close();
                     }
-                }
-            }
-
-            if (checkOtherServer) {
-                serverConnection(ss);
-                for (Socket server : servers) {
-                    System.out.println(server);
-                    DataOutputStream output = new DataOutputStream(server.getOutputStream());
-                    output.writeUTF(str);
-                    server.close();
-                }
-                if (ss.getLocalPort() == portNumber) {
-                    int i = 0;
-                    String answerToClient = "The package died, because the TTL ran out of time...";
-                    while (i<16) {
-                        Socket waitNode = ss.accept();
-                        System.out.println("ECH SINN DOOOOOsadadad");
-                        DataInputStream dis2 = new DataInputStream(waitNode.getInputStream());
-                        String str2 = (String) dis2.readUTF();
-                        System.out.println("Answer from server "+"(i) :"+i+" : " + str2);
-                        i++;
-                        waitNode.close();
-                        if(str2.substring(0, 5).equalsIgnoreCase("FOUND")) {
-                            System.out.println("ECH SINN DOOOOO");
-                            String[] splitMessage2 = str2.split(":");
-                            answerToClient = splitMessage2[1];
-                            break;
-                        }
-                    }
-                    outputStream.writeUTF(answerToClient);
                 }
             }
 
@@ -134,6 +137,7 @@ public class Server implements Runnable {
     private void serverConnection(ServerSocket ss) throws IOException {
         servers.clear();
         int index1 = (Ports.portsList.indexOf(ss.getLocalPort())) + 1;
+        // index to next node (in case it is the last index, it should start at 0 again)
         if (index1 >= Ports.portsList.size()) {
             index1 = 0;
         }
@@ -142,6 +146,7 @@ public class Server implements Runnable {
         servers.add(node1);
         Ports.portsList.indexOf(ss.getLocalPort());
 
+        // since we skip always 2, we need to check for an overflow and manage respectively to the corresponding port
         int index2 = switch ((Ports.portsList.indexOf(ss.getLocalPort())) + 3) {
             case 10 -> 0;
             case 11 -> 1;
@@ -151,7 +156,6 @@ public class Server implements Runnable {
 
         Socket node2 = new Socket("127.0.0.1", Ports.portsList.get(index2));
         servers.add(node2);
-        System.out.println(servers);
     }
 
     @Override
@@ -163,21 +167,13 @@ public class Server implements Runnable {
             port = p;
             try {
                 ss = new ServerSocket(port);
-                // closing the connection so that a new connection can be established later
-                // ss.close();
                 break;
-            } catch (IOException ignored) {
+            } catch (IOException e) {
                 //ignored
             }
         }
+
         System.out.println("Used Port: " + port);
-
-        /*try {
-            Thread.sleep(1000);
-
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }*/
 
         // check if the connection can be done
         try {
